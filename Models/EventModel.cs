d929c7b7daee36934bf0b028f7ecb76ca3abe485
MalekBens <bn.salah.malek@gmail.com>
2022-11-24 18:14:28 +0100

    admin dashboard

---
+++ Models/EventModel.cs
@@ -0,0 +1,16 @@
+namespace FestivalAdminDashboard.Models;
+
+public class EventModel
+{
+    public int? eventID{    get;    set;    }
+    public string? eventName { get; set; }
+    public string? eventDesc { get; set; }
+
+    public string? presenterName { get; set; }
+
+    public string? category { get; set; }
+
+    public string? location { get; set; }
+
+    
+}
